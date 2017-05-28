bd1817fe4f1937758e43c06a430a049ec84c6f2d
olyakorchan <korchanola@gmail.com>
2017-05-28 15:27:26 +0000

    test

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.fbAsyncInit = function () {
     FB.init({
         appId: '299708440455432',
-        cookie: false,
+        cookie: true,
         xfbml: true,
         version: 'v2.9'
     });
@@ -23,19 +23,19 @@ window.fbAsyncInit = function () {
 function LoginToFacebook() {
     var photos = [];
     FB.login(function (response) {
-
-        if (response.authResponse) {
-            console.log('Welcome!  Fetching your information.... ');
-            FB.api('/me', function (response) {
-                console.log('Good to see you, ' + response.name + '.');
-                console.log(response);
-            });
-            FB.api('/me/friends', function (response) {
-                console.log(response.data);
-            });
-        } else {
-            console.log('User cancelled login or did not fully authorize.');
-        }
+console.log(response);
+        // if (response.authResponse) {
+        //     console.log('Welcome!  Fetching your information.... ');
+        //     // FB.api('/me', function (response) {
+        //     //     console.log('Good to see you, ' + response.name + '.');
+        //     //     console.log(response);
+        //     // });
+        //     // FB.api('/me/friends', function (response) {
+        //     //     console.log(response.data);
+        //     // });
+        // } else {
+        //     console.log('User cancelled login or did not fully authorize.');
+        // }
     }, {
         scope: 'email,public_profile,user_friends',
         return_scopes: true
