729b21848a41a3381ef4bc5c6c990daa8c35b66e
olyakorchan <korchanola@gmail.com>
2017-05-28 16:29:34 +0000

    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.fbAsyncInit = function () {
+window.fbAsyncInit = function() {
     FB.init({
         appId: '299708440455432',
         cookie: true,
@@ -8,7 +8,7 @@ window.fbAsyncInit = function () {
     FB.AppEvents.logPageView();
 };
 
-(function (d, s, id) {
+(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {
         return;
@@ -22,30 +22,32 @@ window.fbAsyncInit = function () {
 
 function LoginToFacebook() {
     var photos = [];
-    FB.login(function (response) {
+    FB.login(function(response) {
 
         if (response.authResponse) {
             console.log('Welcome!  Fetching your information.... ');
-            FB.api('/me', function (response) {
+            FB.api('/me', function(response) {
                 console.log('Good to see you, ' + response.name + '.');
             });
-            FB.api('/me/photos?type=uploaded', function (response) {
+            FB.api('/me/photos?type=uploaded', function(response) {
                 var photos = response.data;
-                for (var i = 0; i < photos.length; i++){
-                    var img = document.createElement('img');
-                    img.src = photos[i].picture;
-                    console.log(photos[i].picture)
-                    document.body.appendChild(img);
+                for (var i = 0; i < photos.length; i++) {
+                    FB.api('/' + photos[i].picture + '?fields=picture', function(response) {
+                        var img = document.createElement('img');
+                        img.src = response.picture;
+                        document.body.appendChild(img);
+                    });
                 }
             });
-        } else {
+        }
+        else {
             console.log('User cancelled login or did not fully authorize.');
         }
     }, {
         scope: 'email,public_profile,user_friends',
         return_scopes: true
     });
-    
+
 }
 
 document.getElementById("login").addEventListener("click", LoginToFacebook, false);
