5602a2117af8591a0142488c6c4d4ecae5e6b09e
olyakorchan <korchanola@gmail.com>
2017-05-28 14:12:13 +0000

    test data

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function LoginToFacebook() {
             console.log('Welcome!  Fetching your information.... ');
             FB.api('/me', function (response) {
                 console.log('Good to see you, ' + response.name + '.');
-                console.log("Cover " + response.cover);
+                console.log("Cover " + response);
             });
         } else {
             console.log('User cancelled login or did not fully authorize.');
